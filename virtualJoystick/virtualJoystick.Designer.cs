9c9f421a8684a4b91ab371c951947ff8a9e0fb21
svenhb <svenhb@gmx.de>
2016-06-16 19:50:10 +0000

    Add files via upload

---
+++ virtualJoystick/virtualJoystick.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace virtualJoystick
+{
+    partial class virtualJoystick
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.jogTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // jogTimer
+            // 
+            this.jogTimer.Interval = 500;
+            this.jogTimer.Tick += new System.EventHandler(this.jogTimer_Tick);
+            // 
+            // virtualJoystick
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(400, 400);
+            this.MinimumSize = new System.Drawing.Size(25, 100);
+            this.Name = "virtualJoystick";
+            this.Load += new System.EventHandler(this.virtualJoystick_Load);
+            this.SizeChanged += new System.EventHandler(this.virtualJoystick_SizeChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.virtualJoystick_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.virtualJoystick_MouseDown);
+            this.MouseLeave += new System.EventHandler(this.virtualJoystick_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.virtualJoystick_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.virtualJoystick_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer jogTimer;
+    }
+}
---
+++ virtualJoystick/virtualJoystick.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace virtualJoystick
+{
+    public delegate void JogEventHandler(object sender, JogEventArgs e);
+
+    public partial class virtualJoystick: UserControl
+    {
+        private bool is2d = true;
+        private int jogRaster = 5;
+        private double[] jogValues = new double[] { 0.1, 0.5, 1, 5, 10, 50 };
+        private Color jogColorStandby= Color.Orange;
+        private Color jogColorActive= Color.Red;
+        SolidBrush jogBrush;
+        private SolidBrush jogBrushStandby = new SolidBrush(Color.Orange);
+        private SolidBrush jogBrushActive = new SolidBrush(Color.Red);
+        int jogRadius = 10;
+        int jogPosX, jogPosY;
+        bool jogStart = false;
+
+        public bool Joystick2Dimension
+        {
+            get
+            { return is2d; }
+            set
+            {
+                is2d = value;
+                makeBackgroundPicture();
+            }
+        }
+        public int JoystickRaster
+        {
+            get
+            { return jogRaster; }
+            set
+            {   jogRaster = value;
+                makeBackgroundPicture();
+            }
+        }
+        public Color JoystickActive
+        {
+            get
+            { return jogColorActive; }
+            set
+            {   jogColorActive = value;
+                jogBrushActive = new SolidBrush(jogColorActive);
+            }
+        }
+        public Color JoystickStanby
+        {
+            get
+            { return jogColorStandby; }
+            set
+            {
+                jogColorStandby = value;
+                jogBrushStandby = new SolidBrush(jogColorStandby);
+            }
+        }
+
+        private void virtualJoystick_Paint(object sender, PaintEventArgs e)
+        {
+            int stepX = Width / (2 * jogRaster);
+            int stepY = Height/ (2 * jogRaster);
+            jogRadius = stepY/2;
+            if (is2d)
+                e.Graphics.FillEllipse(jogBrush, new Rectangle(jogPosX - jogRadius, jogPosY - jogRadius, 2 * jogRadius, 2 * jogRadius));
+            else
+                e.Graphics.FillEllipse(jogBrush, new Rectangle(0, jogPosY - jogRadius, Width-2, 2 * jogRadius));
+        }
+
+        System.Drawing.Bitmap jogBackground;
+        private void makeBackgroundPicture()
+        {
+            jogBackground = new System.Drawing.Bitmap(Width,Height);
+            int stepX = Width / (2 * jogRaster);
+            int stepY = Height / (2 * jogRaster);
+            jogRadius = stepX / 2;
+            using (Graphics grp = Graphics.FromImage(jogBackground))
+            {
+                for (int i = 0; i < jogRaster; i++)
+                {
+                    SolidBrush bkgrColor = new SolidBrush(Color.FromArgb(255, 255, 255, i * (255 / jogRaster)));
+                    int locationX = i * stepX;
+                    int locationY = i * stepY;
+                    int sizeX = Width - 2 * locationX;
+                    int sizeY = Height -2 * locationY;
+                    if (!is2d)
+                    {
+                        locationX = 0;
+                        sizeX = Width;
+                    }
+                    grp.FillRectangle(bkgrColor, new Rectangle(locationX, locationY, sizeX, sizeY));
+                    grp.DrawString(jogValues[jogRaster-i-1].ToString(), new Font("Microsoft Sans Serif", stepY/2-2), Brushes.Black, new RectangleF(locationX+ stepX / 8, locationY+ stepY / 8, 5*stepX, stepY));
+                }
+            }
+            this.BackgroundImage = jogBackground;
+        }
+
+        private void virtualJoystick_Load(object sender, EventArgs e)
+        {
+            jogPosX = Width / 2;
+            jogPosY = Height/ 2;
+            jogBrush = jogBrushStandby;
+            makeBackgroundPicture();
+        }
+
+        private void virtualJoystick_MouseDown(object sender, MouseEventArgs e)
+        {
+            jogPosX = jogSetlimit(e.X,Width);
+            jogPosY = jogSetlimit(e.Y,Height);
+            jogBrush = jogBrushActive;
+            jogStart = true;
+            jogTimer.Start();
+            sendPositions();
+            this.Refresh();
+        }
+        private void jogDisable()
+        {   
+            jogPosX = Width / 2;
+            jogPosY = Height/2;
+            jogBrush = jogBrushStandby;
+            jogStart = false;
+            jogTimer.Stop();
+            Refresh();
+        }
+
+        private void virtualJoystick_MouseLeave(object sender, EventArgs e)
+        {
+            jogDisable();
+        }
+
+
+        private void virtualJoystick_MouseUp(object sender, MouseEventArgs e)
+        {
+            jogDisable();
+        }
+
+        private void virtualJoystick_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (jogStart)
+            {
+                jogPosX = jogSetlimit(e.X,Width);
+                jogPosY = jogSetlimit(e.Y,Height);
+            }
+            Refresh();
+        }
+
+        private int jogSetlimit(int jogPos,int limit)
+        {
+            int jogLimit = limit - jogRadius;
+            if (jogPos > jogLimit) { jogPos = jogLimit; }
+            if (jogPos < jogRadius) { jogPos = jogRadius; }
+            return jogPos;
+        }
+
+        protected virtual void jogTimer_Tick(object sender, EventArgs e)
+        {
+            if (jogStart)
+                sendPositions();
+        }
+
+        private void sendPositions()
+        {
+            OnJogTimer(new JogEventArgs(getValue(Width, jogPosX), -1*getValue(Height, jogPosY)));    // new Point(distanceX, distanceY));
+        }
+
+        private double getValue(int max,int pos)
+        {
+            int center = max / 2;
+            int distance = pos - center;
+            int value = Math.Abs(distance);
+            int sign = Math.Sign(distance);
+            int step = max / (2 * jogRaster);
+            if (value < step / 2)
+                return 0.0;
+            int index = value / step;
+            if (index >= jogValues.Length)
+                index = jogValues.Length-1;
+            if (index < 0)
+                index = 0;
+            double dvalue = sign* jogValues[index];
+            return dvalue;
+        }
+
+        public event JogEventHandler JoyStickEvent;
+        private void OnJogTimer(JogEventArgs e)
+        {
+            if (JoyStickEvent != null)
+                JoyStickEvent(this, e);
+        }
+
+        private void virtualJoystick_SizeChanged(object sender, EventArgs e)
+        {
+            makeBackgroundPicture();
+        }
+
+        public virtualJoystick()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class JogEventArgs : EventArgs
+    {
+        private double jogPosX,jogPosY;
+        public JogEventArgs(double posX, double posY)
+        {
+            this.jogPosX = posX;
+            this.jogPosY = posY;
+        }
+        public double JogPosX
+        {   get { return jogPosX; } }
+        public double JogPosY
+        { get { return jogPosY; } }
+    }
+}
